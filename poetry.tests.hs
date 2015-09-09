efd6580793f2adfd2bfd0cf6b5d2f6ca7e702651
Ben Rosenbaum <brosenbaum@learningobjects.com>
2015-09-09 13:57:57 -0400

    some tests

---
+++ poetry.tests.hs
@@ -0,0 +1,9 @@
+
+pattern = "OXOXOX"
+structure = [NS, VS, NS, CONJ, NS, VS, NS]
+
+eclairs = WordData "eclairs" "OX" NS
+provide = WordData "provide" "OX" VS
+frag = [eclairs, provide]
+abhor = WordData "abhor" "OX" VS
+
