34ebc5d1d0e8fbbcc185f52688804fa520e534ca
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2015-09-12 10:14:04 -0400

    now spitting lines, but not finishing its sentences

--- poetry.hs
+++ poetry.hs
@@ -8,6 +8,9 @@ import Data.Monoid
 -- cf. https://en.wikipedia.org/wiki/Arpabet
 -- cf. https://www.wordsapi.com/docs#details
 
+-- needed for random shuffle
+import System.Random
+import qualified Data.Map as Map
 
 type StressPattern = [Char]
 type StressedWrd = (String, StressPattern) 
@@ -356,6 +359,40 @@ abhor = WordData "abhor" "OX" VS
 iambPhrase = isValidForStructures "OXOXOXOX" 
 dactylPhrase = isValidForStructures "XOOXOOXOO"
 
-moreIambs = attemptAddition "OXOXOXOXOX" vocabulary
-bestIambs = last $ takeWhile (\n -> n /= Nothing) $ iterate (>>= moreIambs) (Just frag)
+moreIambs = attemptAddition "OXOXOXOXOX" 
+bestIambs v = last $ takeWhile (\n -> n /= Nothing) $ iterate (>>= moreIambs v) (Just [])
+
+pLineWords n = bestIambs $ shuffle n vocabulary
+pLine n = ( (pLineWords n) >>= Just . unwords .  Data.List.map word)
+poem lines = [ pLine n | n <- [1..lines]]
+readPoem lines = putStrLn $ unwords $ intersperse "\n" $ map (\(Just v) -> v) $ poem lines
+
+
+-- random shuffle, from https://wiki.haskell.org/Random_shuffle, names changed, here because I haven't gotten the hang of importing haskell packages. 
+
+
+ 
+fisherYatesStep :: RandomGen g => (Map.Map Int a, g) -> (Int, a) -> (Map.Map Int a, g)
+fisherYatesStep (m, gen) (i, x) = ((Map.insert j x . Map.insert i (m Map.! j)) m, gen')
+  where
+    (j, gen') = randomR (0, i) gen
+ 
+fisherYates :: RandomGen g => g -> [a] -> ([a], g)
+fisherYates gen [] = ([], gen)
+fisherYates gen l = 
+  toElems $ foldl fisherYatesStep (initial (head l) gen) (numerate (tail l))
+  where
+    toElems (x, y) = (Map.elems x, y)
+    numerate = zip [1..]
+    initial x gen = (Map.singleton 0 x, gen)
+
+shuffle :: Int -> [a] -> [a]
+shuffle i lst = fst $ fisherYates (mkStdGen i) lst
+
+
+
+
+
+
+
 
